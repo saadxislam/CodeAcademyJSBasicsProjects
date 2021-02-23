2b6c23fc245766688ee16b98c0c7d7d22b85cb9e
saadxislam <muhammadsaad1010@gmail.com>
2021-02-22 18:54:39 -0800

    Convert celsius to newton

--- KelvinWeather/app.js
+++ KelvinWeather/app.js
@@ -7,3 +7,9 @@ const celsius = kelvin - 273;
 
 //Formula for converting celsius to F
 let fahrenheit = Math.floor((celsius * (9/5)) + 32);
+
+console.log(`The temperation is ${fahrenheit} degrees Fahrenheit.`);
+
+//Convert celsius to Newton scale
+const newton = Math.floor(celsius * (33/100));
+console.log(`The temperature is ${newton} degrees Newton`);
